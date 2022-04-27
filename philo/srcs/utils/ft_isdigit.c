aec5b0c98bbf3519e5bc4f2c33efbd3cfbd92c0a
増渕美和 <j.aimeparis93@gmail.com>
2022-04-27 10:45:31 +0900

    move func

--- philo/srcs/utils/ft_isdigit.c
+++ philo/srcs/utils/ft_isdigit.c
@@ -8,3 +8,15 @@ int	ft_isdigit(int c)
 	}
 	return (0);
 }
+
+bool	is_digit(char **argv, int i)
+{
+	size_t	j;
+
+	j = 0;
+	while (argv[i][j] && ft_isdigit(argv[i][j]) == 1)
+		j++;
+	if (ft_strlen(argv[i]) != j)
+		return (false);
+	return (true);
+}
