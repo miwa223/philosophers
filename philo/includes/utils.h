2ed1a90b477f20cf3760a3302f39f6e3aedc5f3f
増渕美和 <j.aimeparis93@gmail.com>
2022-04-27 17:10:34 +0900

    move func to utils dir

--- philo/includes/utils.h
+++ philo/includes/utils.h
@@ -2,12 +2,16 @@
 # define UTILS_H
 
 # include <stdlib.h>
+# include <stdbool.h>
+# include <unistd.h>
 # include <limits.h>
 # include <sys/time.h>
 
 int		ft_atoi(char *str, int *overflow);
 size_t	ft_strlen(const char *s);
 int		ft_isdigit(int c);
+bool	is_digit(char **argv, int i);
 long	get_time(void);
+void	sleep_loop(unsigned int	time_to_sleep, long past_time);
 
 #endif
