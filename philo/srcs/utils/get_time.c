f6874c9e9a2b9ce3e0ea78821c80454b2641afb1
増渕美和 <j.aimeparis93@gmail.com>
2022-04-27 10:44:32 +0900

    add usleep func

--- philo/srcs/utils/get_time.c
+++ philo/srcs/utils/get_time.c
@@ -8,3 +8,9 @@ long	get_time(void)
 		return (-1);
 	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
 }
+
+void	sleep_loop(unsigned int	time_to_sleep, long past_time)
+{
+	while (get_time() - past_time < time_to_sleep)
+		usleep(100);
+}
