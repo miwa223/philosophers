22cf875f76c4dbf0f238ae2a519901c52ef3252d
増渕美和 <j.aimeparis93@gmail.com>
2022-04-28 20:22:35 +0900

    move func

--- philo/srcs/action.c
+++ philo/srcs/action.c
@@ -1,22 +1,6 @@
 #include "philo.h"
 #include "utils.h"
 
-long	print_msg(t_thread *philo, char *str)
-{
-	long	now;
-
-	pthread_mutex_lock(&philo->monitor->mutex);
-	if (philo->monitor->end)
-	{
-		pthread_mutex_unlock(&philo->monitor->mutex);
-		return (0);
-	}
-	now = get_usec();
-	printf("%ld %d %s\n", now / 1000, philo->id + 1, str);
-	pthread_mutex_unlock(&philo->monitor->mutex);
-	return (now);
-}
-
 bool	eating(t_thread *philo, long start_time)
 {
 	long	eat_time;
