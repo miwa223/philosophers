ebd6d9e758cc2bc89f1f795e677706885c73cf21
増渕美和 <j.aimeparis93@gmail.com>
2022-04-30 23:49:18 +0900

    modify is_dead.c

--- philo_bonus/srcs/is_dead.c
+++ philo_bonus/srcs/is_dead.c
@@ -3,9 +3,11 @@
 
 bool	is_dead(t_thread *philo)
 {
-	if (philo->data->num == 1
-		|| get_usec() - philo->prev_eat_time > philo->data->die_usec)
+	if philo->data->num == 1
+		|| (get_usec() - philo->prev_eat_time > philo->data->die_usec)
 	{
+		if (philo->data->num == 1)
+			sleep_loop_usec(philo->data->die_usec);
 		sem_wait(philo->monitor->sem_end);
 		printf("%ld %d died\n", get_usec() / 1000, philo->id + 1);
 		return (true);
