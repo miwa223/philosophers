4d25afbd36c38a05db3d2cf865b6da31c442160f
増渕美和 <j.aimeparis93@gmail.com>
2022-04-30 23:51:10 +0900

    correct syntax error

--- philo_bonus/srcs/is_dead.c
+++ philo_bonus/srcs/is_dead.c
@@ -3,11 +3,11 @@
 
 bool	is_dead(t_thread *philo)
 {
-	if philo->data->num == 1
-		|| (get_usec() - philo->prev_eat_time > philo->data->die_usec)
+	if (philo->data->num == 1
+		|| get_usec() - philo->prev_eat_time > philo->data->die_usec)
 	{
 		if (philo->data->num == 1)
-			sleep_loop_usec(philo->data->die_usec);
+			sleep_loop_usec(philo->data->die_usec, philo->prev_eat_time);
 		sem_wait(philo->monitor->sem_end);
 		printf("%ld %d died\n", get_usec() / 1000, philo->id + 1);
 		return (true);
